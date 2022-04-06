0c66d4c330df9eff138b6d94e826d845a4e4adf5
LSD237 <nikitoskravec@mail.ru>
2022-04-06 12:33:26 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import '@styles/scss/styles.scss'
+
+import { upload } from './plugins/upload'
+
+upload('#file')
---
+++ src/plugins/upload.js
@@ -0,0 +1,18 @@
+export function upload(selector) {
+  const input = document.querySelector(selector)
+
+  const open = document.createElement('button')
+  open.classList.add('btn')
+  open.textContent = 'Открыть'
+
+  input.insertAdjacentElement('afterend', open)
+
+  const triggerInput = () => input.click()
+
+  const changeHandler = event => {
+    console.log(event.target.files)
+  }
+
+  open.addEventListener('click', triggerInput)
+  input.addEventListener('change', changeHandler)
+}
