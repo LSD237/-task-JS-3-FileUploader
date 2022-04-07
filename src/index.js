6b3e19887c0695cb988d101ddc75d7fd6d8132cb
LSD237 <nikitoskravec@mail.ru>
2022-04-07 16:41:21 +0200

    first part of basic functionality

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import '@styles/scss/styles.scss'
 
 import { upload } from './plugins/upload'
 
-upload('#file')
+upload('#file', {
+  multi: true, //для добавления множества фай-в
+  accept: ['.png', '.jpg', 'jpeg', '.gif'] //добавлять фай-ы тлько с такими расши-ми
+})
--- src/plugins/upload.js
+++ src/plugins/upload.js
@@ -1,16 +1,52 @@
-export function upload(selector) {
+export function upload(selector, options = {}) {
   const input = document.querySelector(selector)
+  const preview = document.createElement('div') //блок в котором располагаются выбранные картинки
+
+  preview.classList.add('preview')
 
   const open = document.createElement('button')
   open.classList.add('btn')
   open.textContent = 'Открыть'
 
-  input.insertAdjacentElement('afterend', open)
+  if (options.multi) { //если эл-в много - добавляется атри-т позволя-й добавлять много эл-в
+    input.setAttribute('multiple', true) //добавляет аттрибут и его значение
+  }
+
+  if (options.accept && Array.isArray(options.accept)) { //если прису-т параметр accept и это массив
+    input.setAttribute('accept', options.accept.join(','))
+  }
+
+  input.insertAdjacentElement('afterend', preview)
+  input.insertAdjacentElement('afterend', open) //добавляет эл-т open после(afterend) эл-а input
 
   const triggerInput = () => input.click()
 
   const changeHandler = event => {
-    console.log(event.target.files)
+    // console.log(event.target.files)
+    if (!event.target.files.length) { //если не выбранно не одного файла
+      return
+    }
+
+    const files = Array.from(event.target.files) //преобразование к массиву
+
+    files.forEach(file => {
+      if (!file.type.match('image')) { // если это не картинка
+        return
+      }
+
+      const reader = new FileReader()
+
+      reader.onload = ev => {
+        const src = ev.target.result //код картинки в формате Base64
+        preview.insertAdjacentHTML('afterbegin', `
+          <div class="preview-image">
+            <img src="${src}" alt="${file.name}">
+          </div>
+        `)
+      }
+      //это ассинхронное свойство по завершению работы которого вызывается событие onload (выше)
+      reader.readAsDataURL(file)
+    })
   }
 
   open.addEventListener('click', triggerInput)
